59674912aa8e6bf2ca381ab2f451c95c0d165980
Carlos Eduardo <caeca425@gmail.com>
2023-09-05 22:31:19 -0500

    manejo de await y errores que se muestran por pantalla

--- src/components/AjaxComponent.js
+++ src/components/AjaxComponent.js
@@ -5,7 +5,10 @@ import React, { useEffect, useState } from 'react'
 
 export const AjaxComponent = () => {
 
-    const [usurios, setUsuarios] = useState([])
+    const [usurios, setUsuarios] = useState([]);
+    const [cargando, setCargando] = useState([true]);
+    const [errores, setErrores] = useState("");
+
 
 //Generico /basico
 
@@ -37,7 +40,7 @@ const getUsuariosEstaticos = () => {
 
 const getUsuariosAjaxParmas = () => {
 
-    fetch('https://reqres.in/api/users?page=2')
+    fetch('https://reqres.in/api/users?page=1')
         .then(rpta => rpta.json())
         .then(resultadoFinal => {
             setUsuarios(resultadoFinal.data)
@@ -51,21 +54,67 @@ const getUsuariosAjaxParmas = () => {
     ])
 }
 
+const getUsuariosAxajAzyncAwait = () =>{
+    
+    setTimeout(async() => {
+        try {
+            const peticion = await fetch('https://reqres34.in/api/users?page=2');
+            const {data} = await peticion.json();
+            setUsuarios(data);
+            setCargando(false);            
+        } catch (error) {
+            console.log(`Hola ${error.message}`);
+            setErrores(error.message);
+        }
+        
+    }, 3000);
+    
+    //console.log(data);
+}
+
 useEffect(() => {
     //getUsuariosEstaticos()
     getUsuariosAjaxParmas()
-}, [])
+    getUsuariosAxajAzyncAwait()
+}, []);
 
+//Cuando está cargando mostramos esto:
+if(errores !== ""){
+    return(
+        <div className='errores'>
+            {errores}
+        </div>
+        
+    );
+}
+else if(cargando){
+    return(
+        <div className='cargando'>
+            Cargando datos...
+        </div>
+    );
+}
+else if(cargando == false && errores == ""){
+    //Cuando ya carga todo retornamos esto
     return (
         <div>
             <h2>Lista de usuarios via Ajax:</h2>
             <ol className='usuarios'>
                 {
                     usurios.map(usuario => {
-                        return <li key={usuario.id}>{usuario.first_name} {usuario.last_name}<img src={usuario.avatar} /></li>
+                        return <li key={usuario.id}>
+                                <img src={usuario.avatar} width="30" />
+                                &nbsp;
+                                {usuario.first_name} 
+                                {usuario.last_name}
+                            </li>
                     })
                 }
             </ol>
         </div>
     )
+}
+
+
+
 }
